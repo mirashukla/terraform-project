c0ce51b54854eee5c95028a0f26ddbcb9f7f90d9
mirashukla <125200964+mirashukla@users.noreply.github.com>
2023-07-20 10:46:22 +0100

    add web apps to each env

--- managing-environments/global/main.tf
+++ managing-environments/global/main.tf
@@ -1,4 +1,27 @@
+terraform {
+  # Assumes s3 bucket and dynamo DB table already set up
+  # See /code/03-basics/aws-backend
+  backend "s3" {
+    bucket         = "mira-terraform-project-tf-state"
+    key            = "07-managing-multiple-environments/global/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
 # Route53 zone is shared across staging and production
 resource "aws_route53_zone" "primary" {
-  name = "MY_DOMAIN.com"
+  name = "devopsdeployed.com"
 }
